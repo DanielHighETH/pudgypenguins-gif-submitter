b1175cf6ed3c7cfa1f7c75cd5244dbfda6f78929
DanielHighETH <danielhigheth@gmail.com>
2023-11-01 21:00:56 +0700

    renamed getGifs

--- app/api/getGifs/[status]/route.ts
+++ app/api/getGifs/[status]/route.ts
@@ -1,11 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server'
 import connectToDB from '@/app/lib/connectToDb';
 
-
 export async function GET(req: NextRequest, { params }: { params: { status: string } }) {
     const db = await connectToDB('submissions');
     const collection = db.collection('gifs');
-
 
     if(params.status === 'all'){
         const data = await collection.find({}).toArray();
