6737a87240814d6533e00321f56d544d04de5229
DanielHighETH <danielhigheth@gmail.com>
2023-11-01 20:31:29 +0700

    Get all gifs API route

---
+++ app/api/getSubmittedGifs/[status]/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from 'next/server'
+import connectToDB from '@/app/lib/connectToDb';
+
+
+export async function GET(req: NextRequest, { params }: { params: { status: string } }) {
+    const db = await connectToDB('submissions');
+    const collection = db.collection('gifs');
+
+
+    if(params.status === 'all'){
+        const data = await collection.find({}).toArray();
+        return NextResponse.json(data);
+    } else {
+        const data = await collection.find({ status: params.status }).toArray();
+        return NextResponse.json(data);
+    }
+}
