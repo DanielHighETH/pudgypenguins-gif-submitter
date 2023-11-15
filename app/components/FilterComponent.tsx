e7098f270e9d986925a8f18c84a66594283e440c
DanielHighETH <danielhigheth@gmail.com>
2023-11-15 18:38:45 +0700

    Build fix

---
+++ app/components/FilterComponent.tsx
@@ -0,0 +1,30 @@
+'use client'
+import React, { useState, useEffect } from 'react'
+import { FilterProps } from './interfaces';
+
+function FilterComponent({ submissions, setFilteredSubmissions }: FilterProps): React.ReactElement {
+    const [filter, setFilter] = useState('');
+
+    useEffect(() => {
+        const filtered = submissions.filter(submission => 
+            submission.twitterUsername.toLowerCase().includes(filter.toLowerCase()) ||
+            submission.penguinID.toLowerCase().includes(filter.toLowerCase()) ||
+            submission.userWallet.toLowerCase().includes(filter.toLowerCase())
+        );
+
+        setFilteredSubmissions(filtered);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [filter, submissions]);
+
+    return (
+        <input
+            type="text"
+            placeholder="Filter by Username, Penguin ID, or Wallet"
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
+            className="w-full p-2 border-2 border-gray-300 rounded mb-4"
+        />
+    );
+};
+
+export default FilterComponent;
---
+++ app/components/Loader.tsx
@@ -0,0 +1,10 @@
+'use client'
+function Loader() {
+    return (
+    <div className="flex justify-center items-center mt-20 mb-20">
+        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-sky-blue"></div>
+      </div>
+    );
+  };
+  
+  export default Loader;
