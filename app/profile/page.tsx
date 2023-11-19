29cf0ed3e28601fa3e50ed40020420b7ae20e1e8
DanielHighETH <danielhigheth@gmail.com>
2023-11-19 22:03:49 +0700

    Sign out function

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,62 +2,89 @@
 import React from 'react';
 import Link from 'next/link';
 import connectWallet from '../lib/connectWallet';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation'
+
 
 
 function Profile() {
+  const router = useRouter()
+
+  const handleConnectWallet = async () => {
+    try {
+      const connectedAddress = await connectWallet("metamask");
+      if (connectedAddress) {
+        localStorage.setItem("walletAddress", connectedAddress);
+      }
+    } catch (error) {
+      console.error("Error connecting to wallet:", error);
+    }
+  };
+
+  const fetchUserRole = async (address: string) => {
+    try {
+      const response = await fetch(`/api/users/getUserRole/${address}`);
+      if (response.ok) {
+        const data = await response.json();
+        localStorage.setItem("userRole", data.role);
+      }
+    } catch (error) {
+      console.error("Error fetching user role:", error);
+    }
+  };
+
+  useEffect(() => {
+    const savedAddress = localStorage.getItem("walletAddress");
+    if (savedAddress) {
+      fetchUserRole(savedAddress);
+    } else {
+      handleConnectWallet();
+    }
+  }, []);
+
+  const signOut = () => {
+    localStorage.removeItem("walletAddress");
+    localStorage.removeItem("userRole");
+
+    sessionStorage.clear();  
+    
+    window.location.href = "/"; // full refresh to avoid issues with next/link
+};
 
 
-    const handleConnectWallet = async () => {
-        try {
-          const connectedAddress = await connectWallet("metamask");
-          if (connectedAddress) {
-            localStorage.setItem("walletAddress", connectedAddress);
+  const profileLinks = [
+    { title: 'My Submissions', href: `/profile/submissions/`, icon: '🕒' },
+    { title: 'My uploaded GIFs', href: `/profile/uploaded/`, icon: '📤' },
+    { title: 'Sign Out', href: '#', icon: '🚪' }
+  ];
+
+  return (
+    <div className='mx-auto max-w-7xl p-5'>
+      <h1 className='text-5xl text-left mb-10'>Profile</h1>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+        {profileLinks.map((link) => {
+          if (link.title === 'Sign Out') {
+            return (
+              <button key={link.href} onClick={signOut}
+                      className="border rounded p-6 shadow hover:shadow-lg transition-shadow duration-300 flex flex-col items-center justify-center text-2xl">
+                <span>{link.icon}</span>
+                <span>{link.title}</span>
+              </button>
+            );
+          } else {
+            return (
+              <Link key={link.href} href={link.href} 
+                    className="border rounded p-6 shadow hover:shadow-lg transition-shadow duration-300 flex flex-col items-center justify-center text-2xl">
+                <span>{link.icon}</span>
+                <span>{link.title}</span>
+              </Link>
+            );
           }
-        } catch (error) {
-          console.error("Error connecting to wallet:", error);
-        }
-      };
-
-    const fetchUserRole = async (address: string) => {
-        try {
-          const response = await fetch(`/api/users/getUserRole/${address}`);
-          if (response.ok) {
-            const data = await response.json();
-            localStorage.setItem("userRole", data.role);
-          }
-        } catch (error) {
-          console.error("Error fetching user role:", error);
-        }
-      };
-
-      useEffect(() => {
-        const savedAddress = localStorage.getItem("walletAddress");
-        if (savedAddress) {
-          fetchUserRole(savedAddress);
-        } else {
-            handleConnectWallet();
-        }
-      }, []);
-
-    const profileLinks = [
-        { title: 'My Submissions', href: `/profile/submissions/`, icon: '🕒' },
-        { title: 'My uploaded GIFs', href: `/profile/uploaded/`, icon: '📤' },
-    ];
-
-    return (
-        <div className='mx-auto max-w-7xl p-5'>
-            <h1 className='text-5xl text-left mb-10'>Profile</h1>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
-                {profileLinks.map((link) => (
-                    <Link key={link.href} href={link.href} className="border rounded p-6 shadow hover:shadow-lg transition-shadow duration-300 flex flex-col items-center justify-center text-2xl">
-                            <span>{link.icon}</span>
-                            <span>{link.title}</span>
-                    </Link>
-                ))}
-            </div>
-        </div>
-    );
+        })}
+      </div>
+    </div>
+  );
+  
 }
 
 export default Profile;
